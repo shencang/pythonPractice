272fbe218f81f64aaf268eb019d1a44694c8bcba
shencang <2630610009@qq.com>
2018-11-10 19:15:01 +0800

    项目建立

---
+++ test.py
@@ -0,0 +1,13 @@
+print('I \'m "OK"')
+message = "求好运";
+print('1:')
+in1= input(int)
+print('2:')
+in2= input(int)
+name =(int)(in1+in2)
+print(in1,'x',in2,':',name)
+name="ada lovelace"
+print(message)
+print(name.title())
+print(name.upper())
+print(name.lower())
