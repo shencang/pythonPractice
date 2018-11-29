7abdbf1f3546b97089a0bd976cc3a878f33f5009
shencang <2630610009@qq.com>
2018-11-29 17:50:52 +0800

    凸包问题

--- 课程作业/算法分析与设计/实验一：蛮力法的应用/最近对.py
+++ 课程作业/算法分析与设计/实验一：蛮力法的应用/最近对.py
@@ -19,13 +19,13 @@ def closest_points(n, lists_x, list_y, index1, index2):
                 index2 = j
             j = j + 1
         i = i + 1
-    return minDist, index1, index2
+    return np.sqrt(minDist), index1, index2
 
 
 lists_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 4, 5, 5, 3, 4, 3, 6, 9]
 lists_y = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 6, 3, 0, 11, 1, 0, 0, 0, 9]
 
-result = closest_points(20, lists_x, lists_y, 0, 0)
+result = closest_points(len(lists_x), lists_x, lists_y, 0, 0)
 print('最近对为：第', int(result[1]) + 1, '和第', int(result[2]) + 1, '号点。他们之间的距离为：', result[0])
 
 fig, ax = plt.subplots()
@@ -33,9 +33,7 @@ x = np.linspace(0, 2 * np.pi, 200)
 y = np.sin(x)
 l = ax.plot(x, y)
 dot, = ax.plot([], [], 'ro')
-
-
-# line, = ax.plot(x, y)
+line, = ax.plot(x, y)
 
 
 def init():
@@ -68,14 +66,14 @@ def update_line(newl):
 
 ani = animation.FuncAnimation(fig, update_dot, frames=gen_dot, interval=1000, init_func=init)
 # ani = animation.FuncAnimation(fig, update_line, frames = gen_line, interval = 1000, init_func=init)
-plt.title('贝尔斯托迭代法图像（仅绘制实数根）')
+plt.title('蛮力法：最近对问题')
 plt.xlabel("x")
 plt.ylabel("y")
 # plt.xlim(min(lists_x),max(lists_x))# 设置x轴范围
 # plt.ylim(min(lists_y),max(lists_y))# 设置y轴范围
-# s=0
-# while s<10:
-#     plt.scatter(lists_x[s],lists_y[s],c='b')
-#     s= s+1
+s = 0
+while s < len(lists_x):
+    plt.scatter(lists_x[s], lists_y[s], c='b')
+    s = s + 1
 
 plt.show()
