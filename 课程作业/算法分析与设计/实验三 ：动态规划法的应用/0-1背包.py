ea75462c2c9772ea048474a20ee6bc4ab3e25486
shencang <2630610009@qq.com>
2018-12-04 17:22:01 +0800

    改正

--- 课程作业/算法分析与设计/实验三 ：动态规划法的应用/0-1背包.py
+++ 课程作业/算法分析与设计/实验三 ：动态规划法的应用/0-1背包.py
@@ -1,36 +1,26 @@
-import matplotlib.pyplot as plots
-
-VALUE = [([0] * 100) for i in range(100)]
-
-# 用来正常显示负号
-plots.rcParams['axes.unicode_minus'] = False
-# 用来正常显示中文标签
-plots.rcParams['font.sans-serif'] = ['SimHei']
-
-def knapsack(value, weght, capacity, n, choose):
+def knapsack(value, weight, capacity, n, choose):
+    global VALUE
     for i in range(n + 1):
         VALUE[i][0] = 0
     for j in range(capacity + 1):
         VALUE[0][j] = 0
     for i in range(n):
         for j in range(capacity + 1):
-            if (j < weght[i]):
+            if j < weight[i]:
                 VALUE[i][j] = VALUE[i - 1][j]
             else:
-                VALUE[i][j] = max(VALUE[i - 1][j], (VALUE[i - 1][j - weght[i]] + value[i]))
+                VALUE[i][j] = max(VALUE[i - 1][j], (VALUE[i - 1][j - weight[i]] + value[i]))
     j = capacity
     i = n - 1
     while i >= 0:
         if VALUE[i][j] > VALUE[i - 1][j]:
             choose[i] = 1
-            j = j - weght[i]
+            j = j - weight[i]
         else:
             choose[i] = 0
         i = i - 1
 
-    for i in range(n):
-        print(choose[i], ' ', end='')
-    print()
+    display('ch', choose)
     for i in range(n):
         for j in range(capacity + 1):
             print(VALUE[i][j], '\t', end='')
@@ -40,16 +30,20 @@ def knapsack(value, weght, capacity, n, choose):
     return VALUE[n - 1][capacity]
 
 
-def display(flag, a, b):
+def display(flag, a):
     if flag == 'result' or flag == 're':
-        print("拟合的直线为", 'y=', a, '+', b, 'x')
+        print('最佳的价值和是', a)
+    if flag == 'choose' or flag == 'ch':
+        print("选择结果是：", a, '(1代表选择，0代表未选择)')
 
 
+VALUE = [([0] * 20) for i in range(20)]
 if __name__ == '__main__':
-    weight = [2, 2, 6, 5, 4, 3, 5, 6]
-    valve = [6, 3, 5, 4, 6, 4, 5, 7]
-    choose = [0, 0, 0, 0, 0, 0, 0, 0]
-    n = len(weight)
-    capacity = 10
-    sums = knapsack(valve, weight, capacity, n, choose)
-    print(sums)
+    s_weight = [2, 2, 6, 5, 4, 3, 5, 6, 4]
+    s_valve = [3, 6, 5, 4, 6, 4, 5, 7, 9]
+    s_choose = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    s_n = len(s_weight)
+    s_capacity = 10
+    sums = knapsack(s_valve, s_weight, s_capacity, s_n, s_choose)
+    display('re', sums)
