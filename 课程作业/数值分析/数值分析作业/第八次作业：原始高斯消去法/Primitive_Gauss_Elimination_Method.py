775cc394a5b9177a6ea3dd4cb9e2a4c19dd3a89c
shencang <2630610009@qq.com>
2018-12-16 17:58:04 +0800

    数值分析作业8

--- 课程作业/数值分析/数值分析作业/第八次作业：原始高斯消去法/Primitive_Gauss_Elimination_Method.py
+++ 课程作业/数值分析/数值分析作业/第八次作业：原始高斯消去法/Primitive_Gauss_Elimination_Method.py
@@ -1,10 +1,51 @@
 import numpy as np
 
+
+def get_parameters(choose):
+    """
+        选择课本的三组数据中的一组。
+        1.自拟
+        2.ppt数据
+    """
+    if choose == 1:
+        inpa = [[6, 15, 55],
+                [15, 55, 225],
+                [55, 225, 979]]
+        inpb = [152.6, 585.6, 2488.8]
+        return inpa, inpb
+
+    elif choose == 2:
+        inpa = [[1, 2, -3],
+                [0, 5, 2],
+                [-2, 0, -3]]
+        inpb = [8, -4, 2]
+        return inpa, inpb
+    else:
+        print("输入有误。请重新输入")
+        begin()
+        return 0, 0
+
+
 # 求解的数据
-inpa = [[6, 15, 55],
-       [15, 55, 225],
-       [55, 225, 979]]
-inpb = [152.6, 585.6, 2488.8]
+
+
+def input_estimation_and_conditions():
+    """
+    选择参数
+    :return:选择的结果
+    """
+    print('请选择数据:\n---------->1,自拟数据:'
+          '\na = \n[ 6,  15,  55]\n'
+          '[15,  55, 225]\n'
+          '[55, 225, 979]\n'
+          'b = \n[152.6, 585.6, 2488.8]'
+          '\n---------->2,ppt的数据:'
+          '\na = \n[ 1, 2,-3]\n'
+          '[ 0, 5, 2]\n'
+          '[-2, 0,-3]\n'
+          'b = \n[ 8,-4, 2]')
+    result = int(input("==============================\n请输入选择"))
+    return result
 
 
 def swap(cha, chb):
@@ -80,32 +121,28 @@ def gauss_elimination_method(inp, b):
         a, b = exchange_principal(inp, b)
     n = len(inp) - 1
     x = []
-    # while k<(n):
+    # 控制行的移动
     for k in range(1, n):
-        # i= k+1
-        # while i<n+1:
+        # 消去主元对应的其他行的对应变量
         for i in range(k + 1, n + 1):
             factor = inp[i][k] / inp[k][k]
-            # j=k+1
-            # while j<n+1:
+            # 处理列，计算一行内消去主元对应变量后面排列的其他变量的系数和等号右边的常数
             for j in range(k + 1, n + 1):
                 inp[i][j] = inp[i][j] - factor * inp[k][j]
-                j = j + 1
             b[i] = b[i] - factor * b[k]
-            i = i + 1
-        k = k + 1
-    # for i in range(n):
-    #    x.append(b[i]/inp[i][i])
+    # 为解集开辟位置
     for ns in range(n + 1):
         x.append(0)
     x[n] = (b[n] / inp[n][n])
     i = n - 1
+    # 开始储存解集
     while i > 0:
         sums = b[i]
         j = i + 1
         while j < n + 1:
             sums = sums - inp[i][j] * x[j]
             j = j + 1
+        # 保存解集
         x[i] = sums / inp[i][i]
         i = i - 1
     return x
@@ -121,9 +158,45 @@ def draw(a, b):
     # 不会画·····
 
 
-if __name__ == '__main__':
-    a, b = obtain_the_standard_of_ab(inpa, inpb)
-    x = gauss_elimination_method(a, b)
-    draw(a, b)
+def student():
+    """
+    显示学号= =
+    :return: 1607094239 金浩
+    """
+    print('made by 1607094239 金浩')
+
+
+def result(x):
+    """
+    输出结果
+    :param x:
+    :return:
+    """
+    print("解集为：")
     for i in range(1, len(x)):
-        print(x[i])
+        print('x', i, '=', x[i])
+
+
+def begin():
+    """
+    启动方法
+    :return: 没有
+    """
+    # 得到系数和结果矩阵
+    inp_a, inp_b = get_parameters(input_estimation_and_conditions())
+    # 错误输入时推出
+    if inp_a == inp_b and inp_a == 0:
+        exit(0)
+    # 格式化ab
+    a, b = obtain_the_standard_of_ab(inp_a, inp_b)
+    # 高斯消元
+    x = gauss_elimination_method(a, b)
+    # 画图
+    draw(a, b)
+    # 学号输出
+    student()
+    result(x)
+
+
+if __name__ == '__main__':
+    begin()
